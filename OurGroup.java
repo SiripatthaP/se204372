4a79bd0c704871a0d49099fdf5f919348fa88876
Porsche <nattawat.srip@ku.th>
2022-01-12 14:52:05 +0700

    Update OurGroup.java
    เพิ่มชื่อ

--- OurGroup.java
+++ OurGroup.java
@@ -8,6 +8,7 @@ public class OurGroup {
         groupMembers = new Vector<String>();
         groupMembers.add("Siripattha Pinyosamosorn"); //PUT YOUR NAME HERE
         groupMembers.add("Suthida Khrueanak"); //PUT YOUR NAME HERE
+        groupMembers.add("Nattawat Sripatpiriyakul"); //PUT YOUR NAME HERE
     }
     public Vector<String> getGroupMembers () {
         return groupMembers;
